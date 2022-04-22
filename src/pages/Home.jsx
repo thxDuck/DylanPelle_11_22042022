5914b338e5a3a032140200548d38516ca9ff6a66
thxDuck <dylan.pelle.dev@gmail.com>
2022-04-22 14:11:27 +0200

    Initalise scss variables

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,27 @@
-import logo from "../logo.svg";
-import "../App.css";
+import React from "react";
 
-console.log("Hi ! Home");
-
-function Home() {
+export default function Home() {
 	return (
 		<div className="App">
 			<main className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Welcome To Kasa App !</h1>
+				<h1>This is an h1</h1>
+				<h2>This is an h2!</h2>
+				<p className="txt-primary">This is a text with primary color</p>
+				<div
+					className="bg-primary"
+					style={{
+						width: 150,
+						height: 150,
+						padding: 15,
+					}}>
+					<div
+						className="bg-light"
+						style={{
+							width: 50,
+							height: 50,
+						}}></div>
+				</div>
 			</main>
 		</div>
 	);
 }
-
-export default Home;
